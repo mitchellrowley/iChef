0a5e6d66de2035359d48ace143e0773b2396fe13
mitchellrowley <mitchellkrowley1984@gmail.com>
2018-05-23 15:57:10 -0700

    Bad Code
    Code has too many errors with conflicting information.

--- Comprehensive-05-13-2018/Mockup02/eChef.swift
+++ Comprehensive-05-13-2018/Mockup02/eChef.swift
@@ -13,7 +13,7 @@ import AVFoundation
 
 
 
-class Experimental : UIViewController
+class eChef : UIViewController
     
 {
     
@@ -32,53 +32,54 @@ class Experimental : UIViewController
     
     
     var CurrentStep: Int!
-    var textEdit: UITextView!
-   var NextStep: UIButton!
-    func GoToNextStep(_ sender: Any)
+//    @IBOutlet var textEdit: UITextView!
+    @IBOutlet weak var textEdit: UITextView!
+    //    @IBOutlet var NextStep: UIButton!
+    func Speak( _ sender: String )
     {
-    CurrentStep = CurrentStep + 1
-    if ( CurrentStep > NumberSteps - 1 )
-    {
-    CurrentStep = 0
-    
-    }
-    textEdit.text = Steps[CurrentStep]
-    
-    var voiceToUse: AVSpeechSynthesisVoice?
-    for voice in AVSpeechSynthesisVoice.speechVoices() {
-    if #available(iOS 9.0, *) {
-    if voice.name == "Samantha" {
-    voiceToUse = voice
-    }
-    }
-    }
-    _ = AVSpeechSynthesisVoice(identifier: AVSpeechSynthesisVoiceIdentifierAlex)
-    let synth = AVSpeechSynthesizer()
-    let myUtterance = AVSpeechUtterance(string: Steps[ CurrentStep ])
-    myUtterance.voice = voiceToUse
-    myUtterance.rate = 0.5
-    synth.speak(myUtterance)
-    
+        CurrentStep = CurrentStep + 1
+        if ( CurrentStep > NumberSteps - 1 )
+        {
+            CurrentStep = 0
+            
+        }
+        textEdit.text = Steps[CurrentStep]
+        
+        var voiceToUse: AVSpeechSynthesisVoice?
+        for voice in AVSpeechSynthesisVoice.speechVoices() {
+            if #available(iOS 9.0, *) {
+                if voice.name == "Samantha" {
+                    voiceToUse = voice
+                }
+            }
+        }
+        _ = AVSpeechSynthesisVoice(identifier: AVSpeechSynthesisVoiceIdentifierAlex)
+        let synth = AVSpeechSynthesizer()
+        let myUtterance = AVSpeechUtterance(string: sender)
+        myUtterance.voice = voiceToUse
+        myUtterance.rate = 0.5
+        synth.speak(myUtterance)
+        
     }
     
     
     var NumberSteps: Int!
     
     var Steps : [String] = ["Bring a large pot of water to a boil, and salt generously.", "Add the pasta, and boil according to package directions until al dente.", "Strain, and toss with a splash of oil.", "Slice the chicken into 1/4-inch-thick strips, and lay them on a plate or a sheet of waxed paper.", "Season with salt and pepper to taste.", "Heat a large skillet over medium heat, and add 2 tablespoons of the butter.", " When the butter melts, raise the heat to medium-high and add the chicken in 1 layer.", "Cook, without moving the pieces, until the underside has browned, 1 to 2 minutes.", "Flip the pieces, and cook until browned and fully cooked through, 2 to 3 minutes more.", "Transfer the chicken to a medium bowl.", "Reduce the heat to medium, and add the remaining 6 tablespoons butter.", "Scrape the bottom of the skillet with a wooden spoon to release any browned bits.", "When the butter has mostly melted, whisk in the cream and nutmeg and bring to a simmer, then cook for 2 minutes.", "Lower the heat to keep the sauce just warm.", "Whisk the Parmigiano-Reggiano into the sauce.", "Add the chicken and cooked pasta, and toss well.",
-    "Bon Appetit!" ]
+                            "Bon Appetit!" ]
     
-
-   
     
-  
-    @IBAction func PerformRecognition( _ sender: UIButton )
+    
+    
+    
+    func PerformRecognition()
     {
         try! self.startRecording()
-
+        
     }
     
     var transcribed: String!
-    @IBOutlet var button:UIButton!
+//    @IBOutlet var button:UIButton!
     var speechResult = SFSpeechRecognitionResult()
     
     required init?(coder aDecoder: NSCoder) {
@@ -117,12 +118,12 @@ class Experimental : UIViewController
         }
         
         //if addNote {
-       //     recordedTextLabel.text = remove(commands: noteCommands, from: recordedTextLabel.text)
-      //      addNoteTapped(nil)
-      //  } else if addReminder {
-      //      recordedTextLabel.text = remove(commands: remindCommands, from: recordedTextLabel.text)
-      //      addReminderTapped(nil)
-      //  }
+        //     recordedTextLabel.text = remove(commands: noteCommands, from: recordedTextLabel.text)
+        //      addNoteTapped(nil)
+        //  } else if addReminder {
+        //      recordedTextLabel.text = remove(commands: remindCommands, from: recordedTextLabel.text)
+        //      addReminderTapped(nil)
+        //  }
     }
     
     @objc func timerEnded() {
@@ -209,30 +210,32 @@ class Experimental : UIViewController
     
     func recordAndRecognizeSpeech()->Void
     {
-   
+        
     }
     override func viewDidLoad()
     {
         
         super.viewDidLoad()
+        PerformRecognition()
+        Speak (transcribed)
         NumberSteps = Steps.count
         CurrentStep = 0
-                //        guard speechRecogniser.isAvailable
-                //            else
-                //        {
-                //            // Speech recognition is unavailable, so do not attempt to start.
-                //            return
-                //        }
-                //        guard SFSpeechRecognizer.authorizationStatus() == .authorized else {
-                //            SFSpeechRecognizer.requestAuthorization({ (status) in
-                //                // Handle the user's decision.
-                //            })
-                //            return
-                //        }
-                //
-                //       let storyboard = UIStoryboard( name: )
-                
-                // Do any additional setup after loading the view, typically from a nib.
+        //        guard speechRecogniser.isAvailable
+        //            else
+        //        {
+        //            // Speech recognition is unavailable, so do not attempt to start.
+        //            return
+        //        }
+        //        guard SFSpeechRecognizer.authorizationStatus() == .authorized else {
+        //            SFSpeechRecognizer.requestAuthorization({ (status) in
+        //                // Handle the user's decision.
+        //            })
+        //            return
+        //        }
+        //
+        //       let storyboard = UIStoryboard( name: )
+        
+        // Do any additional setup after loading the view, typically from a nib.
         
         // Do any additional setup after loading the view, typically from a nib.
     }
